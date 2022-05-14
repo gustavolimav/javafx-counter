de925468b0b7fc3673a02d99b0e30a3d95ca0db2
gustavolimav <gustavo.lima@liferay.com>
2022-05-14 13:46:26 -0300

    Init

---
+++ src/main/java/com/lima/contador/HelloApplication.java
@@ -0,0 +1,23 @@
+package com.lima.contador;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class HelloApplication extends Application {
+    @Override
+    public void start(Stage primaryStage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        primaryStage.setScene(scene);
+        primaryStage.setTitle("Hello!");
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
